51501c88f0c4f5c15aadbc7e9ded86f3a38c300e
Nalanda Education <nalandaeducation2@gmail.com>
2025-12-23 01:06:42 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-92110331-5cc3d",
+  "appId": "1:771291081316:web:5f4de7be9737541b504d71",
+  "apiKey": "AIzaSyCoHqRO7WE_SzgcNMqGeCmnl_vovfQ7gYA",
+  "authDomain": "studio-92110331-5cc3d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "771291081316"
+};
