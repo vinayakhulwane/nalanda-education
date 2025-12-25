cd0bde407b8efdf961c9a1a5daf05999828f7925
Nalanda Education <nalandaeducation2@gmail.com>
2025-12-25 04:40:21 +0000

    Pls see attached screenshot. Day and dates are not aligned. 30 dec is no

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -33,7 +33,7 @@ function Calendar({
         nav_button_previous: "absolute left-1",
         nav_button_next: "absolute right-1",
         table: "w-full border-collapse space-y-1",
-        head_row: "flex",
+        head_row: "flex justify-between",
         head_cell:
           "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
         row: "flex w-full mt-2",
