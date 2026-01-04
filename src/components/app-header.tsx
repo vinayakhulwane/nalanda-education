c4958461ec2a94e59fba24f5e16c36b63cb7683b
Nalanda Education <nalandaeducation2@gmail.com>
2026-01-04 02:01:34 +0000

    Slide check

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { SidebarTrigger } from "@/components/ui/sidebar";
+
 import { UserNav } from "./user-nav";
 import { useDoc, useFirestore, useUser } from "@/firebase";
 import type { User as AppUser } from "@/types";
@@ -11,18 +11,16 @@ export function AppHeader() {
     const { user } = useUser();
     const firestore = useFirestore();
 
-     const userDocRef = useMemo(() => {
+    const userDocRef = useMemo(() => {
         if (!firestore || !user?.uid) return null;
         return doc(firestore, 'users', user.uid);
     }, [firestore, user?.uid]);
 
     const { data: userProfile, isLoading: isProfileLoading } = useDoc<AppUser>(userDocRef);
 
-
     if (isProfileLoading || !userProfile) {
         return (
-             <header className="sticky top-0 z-10 flex h-16 items-center gap-4 border-b bg-background/80 backdrop-blur-sm px-4 md:px-6">
-                <SidebarTrigger className="md:hidden" />
+            <header className="hidden lg:flex sticky top-0 z-10 h-16 items-center gap-4 border-b bg-background/80 backdrop-blur-sm px-4 md:px-6">
                 <div className="ml-auto">
                     <Skeleton className="h-9 w-9 rounded-full" />
                 </div>
@@ -31,8 +29,7 @@ export function AppHeader() {
     }
 
     return (
-        <header className="sticky top-0 z-10 flex h-16 items-center gap-4 border-b bg-background/80 backdrop-blur-sm px-4 md:px-6">
-            <SidebarTrigger className="md:hidden" />
+        <header className="hidden lg:flex sticky top-0 z-10 h-16 items-center gap-4 border-b bg-background/80 backdrop-blur-sm px-4 md:px-6">
             <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
                 <div className="ml-auto flex-1 sm:flex-initial">
                 </div>
