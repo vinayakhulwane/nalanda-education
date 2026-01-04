e220a8d800c072cd7f092af3ec358a9624acaf91
Nalanda Education <nalandaeducation2@gmail.com>
2026-01-04 01:47:53 +0000

    "I am moving to Phase 3. I need to add Service Worker support to my Next

---
+++ src/next.config.ts
@@ -0,0 +1,43 @@
+/** @type {import('next').NextConfig} */
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  // add your own strategies to the runtime caching list
+  // register: true,
+  // scope: '/app',
+  // sw: 'service-worker.js',
+});
+
+const nextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+module.exports = withPWA(nextConfig);
