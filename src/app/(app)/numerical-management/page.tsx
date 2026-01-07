82760ebe2ec6214676e91f756de4d9d3768c04cc
Nalanda Education <nalandaeducation2@gmail.com>
2026-01-07 13:25:34 +0000

    can we add small margin on both side to this page? (_for element div_)

--- src/app/(app)/numerical-management/page.tsx
+++ src/app/(app)/numerical-management/page.tsx
@@ -69,7 +69,7 @@ export default function NumericalManagementPage() {
     }
 
     return (
-        <div>
+        <div className="p-4 md:p-6">
             <div className="flex justify-between items-center">
                 <PageHeader
                     title="Numerical Management"
